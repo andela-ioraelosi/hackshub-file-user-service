f56822527ea53e38cb534ce6461819172c04032f
Ifeanyi <ifeanyi.oraelosi@andela.co>
2015-04-16 09:08:05 +0100

    Create test specs for User model.
    + Create spec to verify that a new user is added to the 'users' table correctly.
    + Create spec to verify that a user cannot be added without a username.
    + Create spec to verify that a user cannot be added if the username is not unique.
    + Create spec to verify that a user cannot be added without an email.
    + Create spec to verify that a user cannot be added if the email is not unique.
    + Create spec to verify that a user cannot be added without a password.
    + Create spec to verify that a user cannot be added if the password is not unique.
    + Create spec to verify that the user can be updated.
    + Create spec to verify that the user can be deleted.

--- tests/users/models/user.model.spec.js
+++ tests/users/models/user.model.spec.js
@@ -6,7 +6,8 @@ var knex = require('knex')({
   debug: true
 });
 
-var User = require('./../../../app/users/models/user.model');
+var User = require('./../../../app/users/models/user.model')[0];
+var Users = require('./../../../app/users/models/user.model')[1];
 var sampleUser = null;
 var sampleUsers = null;
 
@@ -27,24 +28,233 @@ describe('User Model: ', function () {
 
     // Check that a new user is saved to the database.
     it('should save a new user to the database', function (done) {
-      sampleUser.save().then(function (user) {
-        expect(user.attributes).toEqual(jasmine.objectContaining({
+      sampleUser
+        .save()
+        .then(function (user) {
+          expect(user.toJSON()).toEqual(jasmine.objectContaining({
+            username: 'user1',
+            email: 'userone@users.co',
+            password: '1234',
+            last_name: 'Zer',
+            first_name: 'Uze'
+          }));
+          done();
+        });
+    });
+
+    // Check that the new user cannot be saved if there's no username
+    it('should not save a user without a username', function (done) {
+      sampleUser.set({username: null});
+      sampleUser
+        .save()
+        .then(function (user) {
+        })
+        .catch(function (error) {
+          expect(error).toBeDefined();
+          done();
+        });
+    });
+
+    // Check that the new user cannot be saved if the username already exists
+    it('should not save if username not unique', function (done) {
+      Users
+        .forge([{
           username: 'user1',
           email: 'userone@users.co',
           password: '1234',
           last_name: 'Zer',
           first_name: 'Uze'
-        }));
-        done();
-      });
+        },
+        {
+          username: 'user1',
+          email: 'usertwo@users.co',
+          password: '5678',
+          last_name: 'Zer',
+          first_name: 'Uze'
+        }])
+        .invokeThen('save')
+        .then(function (users) {
+
+        })
+        .catch(function (error) {
+          expect(error).toBeDefined();
+          done();
+        });
     });
 
-    // Check that the new user cannot be saved if there's no username
+    // Check that the new user cannot be saved without setting the email property
+    it('should not save a user without an email', function (done) {
+      sampleUser.set({email: null});
+      sampleUser
+        .save()
+        .then(function (user) {
+        })
+        .catch(function (error) {
+          expect(error).toBeDefined();
+          done();
+        });
+    });
+
+    // Check that the new user cannot be saved if the email already exists
+    it('should not save if email not unique', function (done) {
+      Users
+        .forge([{
+          username: 'user1',
+          email: 'userone@users.co',
+          password: '1234',
+          last_name: 'Zer',
+          first_name: 'Uze'
+        },
+        {
+          username: 'user2',
+          email: 'userone@users.co',
+          password: '5678',
+          last_name: 'Zer',
+          first_name: 'Uze'
+        }])
+        .invokeThen('save')
+        .then(function (users) {
+
+        })
+        .catch(function (error) {
+          expect(error).toBeDefined();
+          done();
+        });
+    });
+
+    // Check that the new user cannot be saved without setting the password property
+    it('should not save a user without an password', function (done) {
+      sampleUser.set({password: null});
+      sampleUser
+        .save()
+        .then(function (user) {
+        })
+        .catch(function (error) {
+          expect(error).toBeDefined();
+          done();
+        });
+    });
+
+    // Check that the new user cannot be saved if the password already exists
+    it('should not save if password not unique', function (done) {
+      Users
+        .forge([{
+          username: 'user1',
+          email: 'userone@users.co',
+          password: '1234',
+          last_name: 'Zer',
+          first_name: 'Uze'
+        },
+        {
+          username: 'user2',
+          email: 'usertwo@users.co',
+          password: '1234',
+          last_name: 'Zer',
+          first_name: 'Uze'
+        }])
+        .invokeThen('save')
+        .then(function (users) {
+
+        })
+        .catch(function (error) {
+          expect(error).toBeDefined();
+          done();
+        });
+    });
 
     afterEach(function (done) {
 
       knex('users')
         .where('username', 'user1')
+        .del()
+        .then(function () {
+          done();
+        });
+    });
+
+  });
+
+  describe('Update users - ', function () {
+
+    beforeEach(function (done) {
+      User
+        .forge({
+          username: 'user3',
+          email: 'userthree@users.co',
+          password: '369',
+          last_name: 'Zer',
+          first_name: 'Uze'
+        })
+        .save()
+        .then(function (user) {
+          done();
+        });
+    });
+
+    it('should update the username', function (done) {
+      User
+        .where({
+          username: 'user3'
+        })
+        .save({
+          username: 'user4'
+        },
+        {
+          method: 'update',
+          patch: true
+        })
+        .then(function (user) {
+          expect(user.toJSON()).toEqual(jasmine.objectContaining({
+            username: 'user4'
+          }));
+          done();
+        });
+
+    });
+
+    afterEach(function (done) {
+      knex('users')
+        .where('password', '369')
+        .del()
+        .then(function () {
+          done();
+        });
+    });
+
+  });
+
+  describe('Delete users - ', function () {
+
+    beforeEach(function (done) {
+      User
+        .forge({
+          username: 'user3',
+          email: 'userthree@users.co',
+          password: '369',
+          last_name: 'Zer',
+          first_name: 'Uze'
+        })
+        .save()
+        .then(function (user) {
+          done();
+        });
+    });
+
+    it('should delete the user', function (done) {
+      User
+        .where({
+          username: 'user3'
+        })
+        .destroy()
+        .then(function (user) {
+          expect(user.toJSON()).toEqual({});
+          done();
+        });
+    });
+
+    afterEach(function (done) {
+      knex('users')
+        .where('password', '369')
         .del()
         .then(function () {
           done();
