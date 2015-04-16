445c0c3e1370bc4faf6d0329a7a8249035b386f0
Ifeanyi <ifeanyi.oraelosi@andela.co>
2015-04-16 20:59:56 +0100

    Implement signup controller for API.
    + Create token on signup.

--- app/users/controllers/user.controller.js
+++ app/users/controllers/user.controller.js
@@ -1,11 +1,79 @@
 'use strict';
+var jwt = require('jsonwebtoken');
+var config = require('./../../../config/config');
+var secret = config.secret;
 
-var UserModel = require('./../models/user.model');
+var User = require('./../models/user.model')[0];
 
 module.exports = (function () {
 
   return {
+    signup: function (request, response) {
+      if (!(request.body.username && request.body.password && request.body.email)) {
+        return response
+          .status(400)
+          .json({
+            type: false,
+            data: 'Incorrect username/password.'
+          });
+      }
 
+        new User({
+          username: request.body.username,
+          password: request.body.password,
+          email: request.body.email
+        })
+        .fetch()
+        .then(function (user) {
+          if (user) {
+            return response
+              .status(400)
+              .json({
+                type: false,
+                data: 'User already exists!'
+              });
+            } else {
+              User
+                .forge({
+                  username: request.body.username,
+                  password: request.body.password,
+                  email: request.body.email,
+                  last_name: request.body.lastname,
+                  first_name: request.body.firstname,
+                  token: '1234'
+                })
+                .save()
+                .then(function (user) {
+                  user
+                    .where({
+                      username: request.body.username
+                    })
+                    .save({
+                      'token': jwt.sign(user, secret)
+                    }, {
+                      method: 'update',
+                      patch: true
+                    })
+                    .then(function (user1) {
+                      response
+                        .json({
+                          type: true,
+                          data: user1,
+                          token: user1.get('token')
+                        });
+                    });
+                });
+            }
+
+        })
+        .catch(function (error) {
+          response
+            .json({
+              type: false,
+              data: 'Error occurred: ' + error
+            });
+        });
+    }
   };
 
 }());
