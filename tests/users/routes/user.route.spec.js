1069e1fa9413eaf95bd5bcb34cd9383f1e75c7f6
Ifeanyi <ifeanyi.oraelosi@andela.co>
2015-04-16 21:26:16 +0100

    Create test specs for API endpoints.
    + Create spec to validate submitted data on new user login.
    + Create spec to verify a correct user is authenticated on login.

--- tests/users/routes/user.route.spec.js
+++ tests/users/routes/user.route.spec.js
@@ -14,6 +14,8 @@ var knex = require('knex')({
 var User = require('./../../../app/users/models/user.model')[0];
 var Users = require('./../../../app/users/models/user.model')[1];
 
+var sampleUser;
+
 describe('User API endpoints: ', function () {
 
   describe('POST /signup - ', function () {
@@ -134,8 +136,104 @@ describe('User API endpoints: ', function () {
     });
   });
 
-  xdescribe('POST /login - ', function () {
+  describe('POST /login - ', function () {
 
+    beforeEach(function (done) {
+
+      sampleUser = User.forge({
+        username: 'user1',
+        email: 'userone@users.co',
+        password: '1234',
+        last_name: 'Zer',
+        first_name: 'Uze',
+        token: 'tolkien'
+      });
+      done();
+    });
+
+    it('should authenticate the user if no username was submitted', function (done) {
+
+      sampleUser
+        .save()
+        .then(function (user) {
+          request(app)
+            .post('/login')
+            .send({username: undefined, password: '1234', email: 'userone@users.co'})
+            .expect(400)
+            .end(function (err, response) {
+              expect(response.body).toEqual(jasmine.objectContaining({
+                data: 'Incorrect username/password.'
+              }));
+              done();
+            });
+        });
+
+    });
+
+    it('should not authenticate the user if no password was submitted', function (done) {
+
+      sampleUser
+        .save()
+        .then(function (user) {
+          request(app)
+            .post('/login')
+            .send({username: 'user1', password: undefined, email: 'userone@users.co'})
+            .expect(400)
+            .end(function (err, response) {
+              expect(response.body).toEqual(jasmine.objectContaining({
+                data: 'Incorrect username/password.'
+              }));
+              done();
+            });
+        });
+
+    });
+
+    it('should authenticate the user and return the token in the response', function (done) {
+
+      sampleUser
+        .save()
+        .then(function (user) {
+          request(app)
+            .post('/login')
+            .send({username: 'user1', password: '1234', email: 'userone@users.co'})
+            .expect(200)
+            .end(function (err, response) {
+              expect(typeof response.body.token).toEqual('string');
+              done();
+            });
+        });
+
+    });
+
+    it('should authenticate the user and return the user data in the response', function (done) {
+
+      sampleUser
+        .save()
+        .then(function (user) {
+          request(app)
+            .post('/login')
+            .send({username: 'user1', password: '1234', email: 'userone@users.co'})
+            .expect(200)
+            .end(function (err, response) {
+              expect(response.body.data).toEqual(jasmine.objectContaining({
+                username: 'user1',
+                email: 'userone@users.co'
+              }));
+              done();
+            });
+        });
+
+    });
+
+    afterEach(function (done) {
+      knex('users')
+        .where(1, 1)
+        .del()
+        .then(function () {
+          done();
+        });
+    });
 
   });
 
