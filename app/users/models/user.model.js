318a52202863d8dbe79266bf1afc1bee0e611410
Ifeanyi <ifeanyi.oraelosi@andela.co>
2015-04-16 09:13:08 +0100

    Implement User model.

--- app/users/models/user.model.js
+++ app/users/models/user.model.js
@@ -1,26 +1,37 @@
 'use strict';
-
 var config = require('./../../../config/config');
 var knex = require('knex')({
   client: config.db[process.env.NODE_ENV].client,
   connection: config.db[process.env.NODE_ENV].connection,
   debug: true
 });
+var bookshelf = require('bookshelf')(knex);
+var tableName = config.db[process.env.NODE_ENV].tableName;
 
-knex.schema.hasTable('users')
+bookshelf.knex.schema.hasTable(tableName)
   .then(function (exists) {
     if (!exists) {
-
+      bookshelf.knex.schema.createTable(tableName, function (table) {
+        table.string('id');
+        table.string('username').primary();
+        table.string('email').notNullable().unique();
+        table.string('password').notNullable().unique();
+        table.string('last_name');
+        table.string('first_name');
+        table.timestamps();
+      })
+      .then(function () {
+        console.log('Schema created.');
+      });
     }
   });
 
-
-
-var bookshelf = require('bookshelf')(knex);
-
 var User = bookshelf.Model.extend({
-  tableName: 'users',
-  hasTimestamps: true
+  tableName: tableName
 });
 
-module.exports = User;
+var Users = bookshelf.Collection.extend({
+  model: User
+});
+
+module.exports = [User, Users];
