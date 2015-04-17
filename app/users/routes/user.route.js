cfc1c276eb2011e266d21be3738f8ae6bc49519e
Ifeanyi <ifeanyi.oraelosi@andela.co>
2015-04-17 17:51:25 +0100

    Define route for fetching users.
    + Create authentication middleware.
    + Use authentication middleware in 'api/v1/users' route.

--- app/users/routes/user.route.js
+++ app/users/routes/user.route.js
@@ -2,8 +2,35 @@
 
 var users = require('./../controllers/user.controller');
 
+
+function ensureAuthorized (request, response, next) {
+  var bearerToken;
+  var bearerHeader = request.headers['authorization'];
+  if (typeof bearerHeader !== 'undefined') {
+    bearerToken = bearerHeader;
+    request.token = bearerToken;
+    next();
+  } else {
+    response
+      .status(403)
+      .json({
+        type: false,
+        data: 'Unauthorized.'
+      });
+  }
+}
+
 module.exports = function (router) {
 
   router.route('/signup')
     .post(users.signup);
+
+  router.route('/login')
+    .post(users.login);
+
+  router
+    .use('/api/v1/users', ensureAuthorized);
+
+  router.route('/api/v1/users')
+    .get(users.getAllUsers);
 };
